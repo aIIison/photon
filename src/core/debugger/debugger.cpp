efdfd52f3eb0d3e06c6698c45bee495034fe6b2d
hero <komocsinlaci@gmail.com>
2025-11-15 17:48:28 +0100

    snprintf

--- src/core/debugger/debugger.cpp
+++ src/core/debugger/debugger.cpp
@@ -17,8 +17,8 @@ void debugger::disasm( uintptr_t addr ) {
 		char hex_buf[ 64 ] = { 0 };
 		int  hex_pos       = 0;
 		for ( ZyanU8 i = 0; i < insn.info.length; i++ ) {
-			hex_pos += sprintf_s( hex_buf + hex_pos, sizeof( hex_buf ) - hex_pos,
-			                      "%02X ", ( ( unsigned char* ) addr )[ i ] );
+			hex_pos += snprintf( hex_buf + hex_pos, sizeof( hex_buf ) - hex_pos,
+			                     "%02X ", ( ( unsigned char* ) addr )[ i ] );
 		}
 		util::console::log( PRINT_CYAN "%p    " PRINT_BLUE "%-40s" PRINT_YELLOW "%s\n", addr, hex_buf, insn.text );
 
